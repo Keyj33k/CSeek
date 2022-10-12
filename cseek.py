6693bbc1f56b69bb750f5cc58f76ef4004b352e5
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-12 21:46:01 +0200

    update to v0.0.5
    - removed/changed config messages
    - updated coloring
    - changed error messages

--- cseek.py
+++ cseek.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from argparse import ArgumentParser, SUPPRESS
+from argparse import ArgumentParser, SUPPRESS, HelpFormatter
 from socket import socket, AF_INET, SOCK_STREAM, getservbyport, gethostbyaddr, herror
 from subprocess import check_output, CalledProcessError
 from time import strftime
@@ -10,30 +10,28 @@ from datetime import datetime
 
 """
 cseek - Client Identifier
+
 Ping ranges of ipv4 addresses to get the status and if enabled scans for open ports
 from the current address.
+
 Author: Keyjeek
 Date: 18.09.22
-Version: 0.0.4
+Version: 0.0.5
 """
 
-RED = "\033[0;31m"
-GREEN = "\033[0;32m"
-RESET = "\033[0m"
+color = {
+    "R": "\033[0;31m",
+    "Y": "\033[0;93m",
+    "G": "\033[0;92m",
+    "D": "\033[0m"
+}
 
 def host_conf_check(b_host: int, l_host: int):
-    if int(b_host) <= 0 or int(l_host) <= 0:
-        print(f"host check: {RED}FALSE{RESET}")
-        return False
-    else:
-        print(f"host check: {GREEN}OK{RESET}")
-        return True
-
-def port_check_outp(port: int):
-    exit(f"cseek: port check: port {port} {RED}FALSE{RESET}")
+    if int(b_host) <= 0 or int(b_host) >= 255 or int(l_host) <= 0 or int(l_host) >= 255:
+        exit(f"cseek: invalid config, please check your host configs")
 
 def write_outp_p(port: int, service: str):
-    print(f" |\tproto=TCP, port={port}, status=open, service={service}")
+    print(f" |\t+ proto=TCP, port={port}, status=open, service={service}")
     with open("output/cseek_output.txt", 'a') as write_output:
         write_output.write(f" |\tproto=TCP, port={port}, status=open, service={service}\n")
 
@@ -61,27 +59,25 @@ class CSeek:
             mkdir("output")
             with open("output/cseek_output.txt", 'w') as output_file:
                 output_file.write("cseek - output file\n")
-            print(f"output file check: {GREEN}OK{RESET}")
         except (FileExistsError, OSError):
-            print(f"output file check: {GREEN}OK{RESET}")
+            pass
 
     def port_check(self):
+        ports = [self.begin_port, self.final_port]
+
         if self.begin_port > self.final_port:
-            exit(f"cseek: port check: {RED}FALSE{RESET}")
-        elif self.begin_port >= 65534 or self.begin_port <= 0:
-            port_check_outp(self.begin_port)
-        elif self.final_port >= 65535 or self.final_port <= 0:
-            port_check_outp(self.final_port)
-        else:
-            print(f"port check: {GREEN}OK{RESET}")
+            exit(f"cseek: {color['R']}invalid config{color['D']}, start host cannot be bigger then the last host")
+
+        for port in ports:
+            if port >= 65534 or port <= 0: exit(f"cseek: {color['R']}invalid config{color['D']}, "
+                                                f"port {port} is invalid")
 
     def octet_check(self):
         split_address = self.target_address.split(".")
         for octet in range(3):
             if int(split_address[octet]) <= 0 or int(split_address[octet]) >= 253:
-                exit(f"octet check: octet {octet + 1} ( {split_address[octet]} ) {RED}FALSE{RESET}")
-            else:
-                print(f"octet check: {octet + 1} {GREEN}OK{RESET}")
+                exit(f"cseek: {color['R']}invalid config{color['D']}, octet {octet + 1} "
+                     f"( {split_address[octet]} ) is incorrect")
 
     def scan_port_range(self, target_address):
         open_ports = 0
@@ -97,7 +93,6 @@ class CSeek:
 
         port_range = self.final_port - self.begin_port
         closed_ports = port_range - open_ports
-
         print(f" |  port scan done: total={port_range} open={open_ports} closed={closed_ports}")
 
     def ping_target(self):
@@ -112,17 +107,19 @@ class CSeek:
 
             try:
                 check_output(["ping", "-c", str(self.ping_count), final_address])
-                print((f"[{GREEN}+{RESET}] {final_address} ( {GREEN}{gethostbyaddr(final_address)[0]}{RESET} ): "
-                       f"{GREEN}connected successfully{RESET}, count={scan_count}, time={strftime('%H:%M:%S')}"))
+                print(f"[{color['G']}+{color['D']}] {final_address} ( hostname: {gethostbyaddr(final_address)[0]} ): "
+                      f"{color['G']}connected successfully{color['D']}, count={scan_count}, "
+                      f"time={strftime('%H:%M:%S')}")
                 write_outp_i(final_address, gethostbyaddr(final_address)[0], scan_count)
                 active_host_count += 1
                 if self.activate_port_scan is not False: cseek.scan_port_range(final_address)
             except CalledProcessError:
-                print(f"{final_address}: {RED}connection failed{RESET}, "
+                print(f"{final_address}: {color['R']}connection failed{color['D']}, "
                       f"count={scan_count}, time={strftime('%H:%M:%S')}")
             except herror:
-                print((f"[{GREEN}+{RESET}] {final_address} ( {RED}unknown{RESET} ): {GREEN}connected "
-                       f"successfully{RESET}, count={scan_count}, time={strftime('%H:%M:%S')}"))
+                print(f"[{color['G']}+{color['D']}] {final_address} ( {color['R']}unknown{color['D']} ): "
+                      f"{color['G']}connected successfully{color['D']}, count={scan_count}, "
+                      f"time={strftime('%H:%M:%S')}")
                 write_outp_i(final_address, "unknown", scan_count)
 
         inactive_hosts = host_count - active_host_count
@@ -130,17 +127,18 @@ class CSeek:
         min_address = f"{self.target_address}.{self.begin_host}"
         max_address = f"{self.target_address}.{self.final_host}"
 
-        print("\n***************** statistics *****************")
-        print((f"total={host_count} active={active_hosts} "
+        print(f"\n{'*' * 15} statistics {'*' * 17}\n"
+               f"total={host_count} active={active_hosts} "
                f"inactive={inactive_hosts} min={min_address}\n"
-               f"max={max_address} runtime={datetime.now() - scan_start}"))
+               f"max={max_address} runtime={datetime.now() - scan_start}")
 
 
 if __name__ == "__main__":
     def headline(mode: str):
-        print(f"\ncseek ( 0.0.4 ) start {mode} scan at {datetime.now()} ...\n")
+        print(f"cseek - v0.0.5 ( https://github.com/Keyj33k/CSeek )\n\nstart {mode} scan at {datetime.now()} ...\n")
 
-    parser = ArgumentParser(description="cseek - Network Client Identifier")
+    parser = ArgumentParser(description="cseek - Network Client Identifier",
+                            formatter_class=lambda prog: HelpFormatter(prog, max_help_position=37))
     parser.add_argument("-v", "--version", action="version",
                         version="cseek - Client Identifier, Version 0.0.3", help=SUPPRESS)
     parser.add_argument("-u", "--unlock", help="unlock port scanning", action="store_true")
@@ -156,23 +154,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
     unlock_flag = vars(args)["unlock"]
 
-    if int(args.begin) >= 253 or int(args.final) >= 254:
-        exit(f"host config check: {RED}FALSE{RESET}")
-    elif int(args.begin) > int(args.final):
-        exit(f"host check: {RED}FALSE{RESET}")
-    elif (unlock_flag is False and vars(args)["start"] is not None
-          or unlock_flag is False and vars(args)["last"] is not None):
-        exit(f"port config check: {RED}FALSE{RESET}")
+    if (unlock_flag is False and vars(args)["start"] is not None
+        or unlock_flag is False and vars(args)["last"] is not None):
+        exit(f"cseek: {color['R']}invalid config{color['D']}, perhaps you forgot the unlock flag?")
     elif (unlock_flag is True and vars(args)["start"] is None
           or unlock_flag is True and vars(args)["last"] is None):
-        exit(f"port config check: {RED}FALSE{RESET}")
+        exit(f"cseek: {color['R']}invalid config{color['D']}, you cannot activate "
+             "the port scan without the scan configs")
 
     try:
-        cseek = CSeek(args.addr, args.begin, args.final, args.start,
-                      args.last, args.unlock, args.count)
-        print("running checks ...\n")
+        cseek = CSeek(args.addr, args.begin, args.final, args.start, args.last, args.unlock, args.count)
         cseek.create_output_file()
-        if host_conf_check(args.begin, args.final) is not True: exit(1)
+        host_conf_check(args.begin, args.final)
         if unlock_flag is True: cseek.port_check()
         cseek.octet_check()
         headline("extended") if args.unlock is not False else headline("basic")
@@ -184,7 +177,7 @@ if __name__ == "__main__":
                "  cseek.py -a 192.168.2 -b 1 -f 100 -c 1 "))
         exit(1)
     except TypeError:
-        print((f'\n{RED}interrupted{RESET}: cannot process because of invalid configurations\n'
-               'type "python3 cseek.py -h" to get more informations'))
+        print(f'{color["R"]}interrupted{color["D"]}: cannot process because of invalid configurations\n'
+              'type "python3 cseek.py -h" to get more informations')
     except KeyboardInterrupt:
-        print("\ncseek exits due interruption")
+        print(f"\n{color['Y']}cseek exits due interruption")
